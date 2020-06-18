35ef191bb6bd94a2e29ee4d8bc2a422d27e623f6
kazk <kazk.dev@gmail.com>
2020-06-18 00:02:59 -0700

    Add current version
    Slightly cleaned up and renamed the files.

---
+++ display.rb
@@ -0,0 +1,41 @@
+$stdout.sync = true
+require 'json'
+
+class Display
+  class << self
+    def table(data, label: "Table", tab: false, allow_preview: false)
+      print(tab ? "TAB" : "LOG", data.to_json, mode: "TABLE", label: label)
+      prop("preview", true) if allow_preview and data.count > 10
+    end
+
+    def log(msg, label = "", mode = "")
+      print('LOG', msg, mode: mode, label: label)
+    end
+
+    def print(type, msg, mode: "", label: "")
+      puts format_msg("<#{type.upcase}:#{mode.upcase}:#{label}>#{msg}")
+    end
+
+    def prop(name, value)
+      puts format_msg("<PROP::#{name}>#{value}")
+    end
+
+    def format_msg(msg)
+      msg.gsub("\n", '<:LF:>')
+    end
+
+    def status(msg)
+      print("STATUS", msg)
+    end
+
+    def pre(content, label = nil)
+      log("<pre>#{content}</pre>", label)
+    end
+
+    alias :raw :pre
+
+    def html_tag(tag, content)
+      puts "<#{tag}>#{content}</#{tag}>"
+    end
+  end
+end
